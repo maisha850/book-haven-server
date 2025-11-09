4e4b9bf135aa58b4b194c4ff2ed363cec33dca78
maisha850 <tasnimmaisha08@gmail.com>
2025-11-09 13:49:28 +0600

    added books collection

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express()
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 3000
+app.use(cors())
+app.use(express.json())
+
+const uri = "mongodb+srv://bookHaven:ciZnOzzKdbPpX1si@cluster0.6aaggy0.mongodb.net/?appName=Cluster0";
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+app.get('/' , (req , res)=>{
+    res.send('book haven server is running')
+})
+
+async function run() {
+  try {
+  await client.connect();
+const db = client.db('books_haven')
+const booksCollection = db.collection('books')
+app.get('/books' , async(req , res)=>{
+  const cursor = booksCollection.find()
+  const result = await cursor.toArray()
+  res.send(result)
+})
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+app.listen(port , ()=>{
+    console.log(`book haven server is running on port: ${port}`)
+})
+
