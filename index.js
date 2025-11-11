fe210ed283fe852d86b89df58cec7ee522a44162
maisha850 <tasnimmaisha08@gmail.com>
2025-11-11 14:53:42 +0600

    added update and delete functionalities

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const app = express()
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const admin = require("firebase-admin");
+const serviceAccount = require("./book-haven-firebase-adminsdk.json");
 const port = process.env.PORT || 3000
 app.use(cors())
 app.use(express.json())
+
+
+
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+
 
 const uri = "mongodb+srv://bookHaven:ciZnOzzKdbPpX1si@cluster0.6aaggy0.mongodb.net/?appName=Cluster0";
 const client = new MongoClient(uri, {
@@ -14,6 +25,22 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
+
+const verifyToken = async(req , res , next)=>{
+  const authorization = req.headers.authorization
+  if(!authorization){
+    return res.status(401).send({message: 'unauthorized access'})
+  }
+  const token = authorization.split(' ')[1]
+  try{
+   await admin.auth().verifyIdToken(token)
+   next()
+  }
+  catch(error){
+    return res.status(401).send({message: 'unauthorized access'})
+  }
+
+}
 app.get('/' , (req , res)=>{
     res.send('book haven server is running')
 })
@@ -33,6 +60,28 @@ app.post('/books' , async(req , res)=>{
   console.log(data)
   const result = await booksCollection.insertOne(data)
   res.send(result)
+})
+app.get('/books/:id', verifyToken, async(req , res)=>{
+  const id = req.params.id
+  const query = {_id : new ObjectId(id)}
+  const result = await booksCollection.findOne(query)
+  res.send(result)
+})
+app.put('/books/:id', verifyToken ,async(req , res)=>{
+  const id = req.params.id
+  const data =req.body;
+  const query = {_id : new ObjectId(id)}
+  const update = {
+    $set: data
+  }
+  const result = await booksCollection.updateOne(query , update) 
+  res.send(result)
+})
+app.delete( '/books/:id' , verifyToken , async(req , res)=>{
+ const id = req.params.id;
+ const query = { _id: new ObjectId(id)}
+ const result = await booksCollection.deleteOne(query)
+ res.send(result)
 })
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
